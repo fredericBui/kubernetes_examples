0d68c4e5613b0b1f83c19e4b63870f95782aa65f
Frederic_Bui <bui.frederic.pro@gmail.com>
2024-12-11 23:50:47 +0100

    Create files to illustrate deployment with secret

---
+++ deployment-with-secret-env/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Démo env</title>
+</head>
+<body>
+    <h1>Hello <?php echo(getenv('MY_ENV')) ?></h1>
+</body>
+</html>
